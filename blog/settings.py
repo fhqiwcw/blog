512bb70eb127162eac1666e3c99da29e228a97db
fhqiwcw <fhqiwcw@gmail.com>
2012-04-25 00:42:09 +0800

    set

--- blog/settings.py
+++ blog/settings.py
@@ -59,7 +59,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'G:/pythonWorkspace/eclipse/blog/static/'
+STATIC_ROOT = 'G:/pythonWorkspace/eclipse/myblog/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -67,7 +67,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-                    'G:/pythonWorkspace/eclipse/blog/templates/static/',
+                    'G:/pythonWorkspace/eclipse/myblog/templates/static/',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -117,7 +117,7 @@ ROOT_URLCONF = 'blog.urls'
 WSGI_APPLICATION = 'blog.wsgi.application'
 
 TEMPLATE_DIRS = (
-     'G:/pythonWorkspace/eclipse/blog/templates'
+     'G:/pythonWorkspace/eclipse/myblog/templates'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
